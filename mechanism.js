b4ccee93200dc7f3a2a224fc25db08ad8a9378f0
thedevyansh <schawla333333@gmail.com>
2020-04-09 23:49:21 +0530

    updated

---
+++ mechanism.js
@@ -0,0 +1,79 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess()
+{
+	let userGuess = Number(guessField.value);
+
+    if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+  }
+
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations!';
+    lastResult.style.backgroundColor = 'rgb(157,193,131)';
+    lastResult.style.color = 'black';
+    lowOrHi.textContent = '';
+    setGameOver();
+
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    setGameOver();
+
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'rgba(188,85,75,0.5)';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+ 
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = '#20011E';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
